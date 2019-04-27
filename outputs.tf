a79a2632b8e12cdfdb8bbcf6009f8eae34465110
Alvin Savoy <asavoy@users.noreply.github.com>
2019-04-27 20:35:18 +1000

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,21 @@ output "ecs_service_arn" {
   value       = "${aws_ecs_service.ecs_service.id}"
 }
 
+output "task_definition_arn" {
+  description = "ARN of the ECS task definition"
+  value       = "${aws_ecs_task_definition.ecs_task.arn}"
+}
+
+output "task_execution_role_arn" {
+  description = "ARN of the ECS task execution role"
+  value       = "${aws_iam_role.ecs_task_execution_role.arn}"
+}
+
+output "task_role_arn" {
+  description = "ARN of the ECS task role"
+  value       = "${aws_iam_role.ecs_task_role.arn}"
+}
+
 output "ecs_task_role_id" {
   description = "ID for the ECS task role, to attach a custom policy"
   value       = "${aws_iam_role.ecs_task_role.id}"
